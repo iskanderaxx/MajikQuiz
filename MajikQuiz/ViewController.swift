4e1c97e9a17b01a95a2c931621038fee3052223a
Alexander Stepanov <alexanderlawyer97@gmail.com>
2025-02-26 21:16:45 +0400

    Initial Commit

---
+++ MajikQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MajikQuiz
+//
+//  Created by Mac Alexander on 26.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
